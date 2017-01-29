c89283f35a66de2e5f539659d6d92c25ea3a4835
daxapps <daxapps777@gmail.com>
2017-01-29 15:51:54 -0600

    Initial Commit

---
+++ Properties/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Properties
+//
+//  Created by Jason Crawford on 1/29/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Properties/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Properties
+//
+//  Created by Jason Crawford on 1/29/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
